758e6ceeead59ea211d5d327bbad5ed9c7a3fa80
Abhishek Kumar <132087951+abhi9504@users.noreply.github.com>
2026-08-21 17:26:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
-        // Base Case
-        if(n == 1 || n == 2) return n;
-
-        // check already exist condition
-        if(dp[n] != -1)  return dp[n];
-
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    }
     int climbStairs(int n) {
-        // Q2. Day 5/30
-        vector<int> dp(n+1, -1);
+      // By Tabulation Method  
+      int dp[n+1];  
+      dp[0] = 1;
+      dp[1] = 1;
 
-        return solve(n, dp);
+     for(int i=2; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+     }   
+     return dp[n];
     }
 };
