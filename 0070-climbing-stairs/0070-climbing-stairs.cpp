7a9cc07abeec0465b8e1e07266000162b8fa461c
Abhishek Kumar <132087951+abhi9504@users.noreply.github.com>
2026-09-17 17:46:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-      // By Tabulation Method  
-      int dp[n+1];  
-      dp[0] = 1;
-      dp[1] = 1;
+    int solve(int n, vector<int>& dp) {
+        // Base Case
+        if(n == 1 || n == 2) return n;
 
-     for(int i=2; i<=n; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
-     }   
-     return dp[n];
+        // step 2 check already exist condtion
+        if(dp[n] != -1)  return dp[n];
+
+        // step 3 return karane se phle usee dp mai store karao
+        return dp[n] = solve(n-1, dp) +  solve(n-2, dp);
+    }
+    int climbStairs(int n) {
+        // Abhi Code Karo
+        // step1 create dp array n+1 size & initialize with -1;
+        vector<int> dp(n+1, -1);
+
+       return solve(n, dp);
     }
 };
