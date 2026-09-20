203eeb1e074368f559dc180748eb2a6a9b3481c6
Abhishek Kumar <132087951+abhi9504@users.noreply.github.com>
2026-09-21 04:29:01 +0530

    Time: 0 ms (100%), Space: 27.4 MB (15.57%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    void solve(int st, int e, vector<char>& s) {
-        // Base Case
-        if(st > e)  return;
-        swap(s[st], s[e]);
-        solve(st+=1, e-=1, s);
-    }
     void reverseString(vector<char>& s) {
-        // Using Recursion
-        int n = s.size();
-        solve(0, n-1, s);
+      // Abhi Code Karo
+      // Using Two ptr
+      int n = s.size();
+      int left = 0;
+      int right = n-1;
+      while(left <= right) {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+      }  
     }
 };
