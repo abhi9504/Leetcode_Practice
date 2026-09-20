d82421c33eaf8f1c26c5a0e9dc90c2e45f29cc58
Abhishek Kumar <132087951+abhi9504@users.noreply.github.com>
2026-09-21 04:34:10 +0530

    Time: 0 ms (100%), Space: 9.9 MB (52.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = s.size();
-        int i = 0;
-        int j =  n-1;
-
-        while(i < j) {
-            if(!isalnum(s[i])) i++;
-            else if(!isalnum(s[j])) j--;
-            else if(tolower(s[i]) == tolower(s[j]))  i++, j--;
-            else return false;
-        }
-        return true;
+    // Abhi Code karo
+    // Using Two ptr
+    int n = s.size();
+    int i = 0;
+    int j = n-1;
+    while(i < j) {
+        if(!isalnum(s[i])) i++;
+        else if(!isalnum(s[j])) j--;
+        else if(tolower(s[i]) == tolower(s[j])) i++, j--;
+        else return false;
+    }
+     return true;
     }
 };
