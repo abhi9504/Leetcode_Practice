b8118e9ecc03e381eaeef2a98edad66183779086
Abhishek Kumar <132087951+abhi9504@users.noreply.github.com>
2026-09-24 14:26:04 +0530

    Time: 1 ms (59.25%), Space: 30.2 MB (66.54%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        // Abhi Code Karo
-        // solve using 3 ptr
-        int n = nums.size();
-        vector<int> ans(n);
-        int s = 0;
-        int e = n-1;
-        int idx = n-1;
+       // Abhi Code Karo
+       int n = nums.size();
+       vector<int> ans(n);
+       int s = 0;
+       int e = n-1;
+       int idx = n-1;
 
-        while(s <= e) {
-            int sSquare = nums[s] * nums[s];
-            int eSquare = nums[e] * nums[e];
+       while(s <= e) {
+        long long int sSq = nums[s] * nums[s];
+        long long int eSq = nums[e] * nums[e];
 
-            if(eSquare > sSquare) {
-                ans[idx] = eSquare;
-                e--;
-            }
-            else {
-                ans[idx] = sSquare;
-                s++;
-            }
+        if(eSq > sSq) {
+            ans[idx] = eSq;
+            e--;
             idx--;
         }
-        return ans;
+        else {
+            ans[idx] = sSq;
+            s++;
+            idx--;
+        }
+       } 
+       return ans;
     }
 };
